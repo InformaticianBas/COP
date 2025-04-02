a3867895fc0b0c0b6852080c921c9b897a4a190c
Bas M <cheminduloup@protonmail.com>
2025-04-02 11:13:01 -0400

    fixing 42

--- Module_4/42.py
+++ Module_4/42.py
@@ -3,13 +3,13 @@ employee_name = input("Enter Employee Name: ")
 number_of_shifts = input ("Enter number of shifts: ")
 number_of_transactions = input("Enter number of transactions: ")
 transaction_dollar_value = input("Enter transaction dollar value: ")
-productivity_score = ((int(transaction_dollar_value)/int(number_of_transactions))/int(number_of_shifts))
+productivity_score = int((float(transaction_dollar_value)/float(number_of_transactions))/float(number_of_shifts))
 bonus = 0.00
 
 # Calculate bonus with nested ifs
 if (productivity_score > 30 ):
     if (productivity_score > 70):
-        if (productivity_score > 200):
+        if (productivity_score >= 200):
             bonus = 200.00
         else:
             bonus = 100.00
