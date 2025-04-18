b68b806766b127fd3d3397b3e5d72594abea1ccf
Bas M <cheminduloup@protonmail.com>
2025-04-18 11:25:02 -0400

    Added Projects

---
+++ Project/Project02.py
@@ -0,0 +1,19 @@
+AllowedVehiclesList = [ 'Ford F-150', 'Chevrolet Silverado', 'Tesla CyberTruck', 'Toyota Tundra', 'Nissan Titan' ]
+print('********************************\nAutoCountry Vehicle Finder v0.1\n********************************\nPlease Enter the following number below from the following menu:\n\n1. PRINT all Authorized Vehicle\n2. SEARCH for Authorized Vehicle\n3. Exit\n')
+
+menu_number = int(input())
+if menu_number == 1:
+    print('The AutoCountry sales manager has authorized the purchase and selling of the following vehicles:')
+    for make in AllowedVehiclesList:
+        print(make)
+elif menu_number == 2:
+    search_field = str(input())
+    if search_field in AllowedVehiclesList:
+        print(search_field,'is an authorized vehicle.')
+    else:
+        print(search_field,'is not an authorized vehicle, if you received this in error please check the spelling and try again')
+    
+elif menu_number == 3:    
+    print('Thank you for using the AutoCountry Vehicle Finder, good-bye!')
+else: # Extra that I added
+    print('Invalid number. Try again.')
